b27f9e7bd32cb4593cd2f229659c07007434634a
Lasse Westerberg <westerberg.lasse@gmail.com>
2012-09-17 01:12:40 +0200

    Pagination Test 2 - missing dll
    Spent a couple of hours searching for an answer, no luck.

--- SportsStore/SportsStore.UnitTests/PaginationTest.cs
+++ SportsStore/SportsStore.UnitTests/PaginationTest.cs
@@ -2,7 +2,7 @@
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web.Mvc;
+using System.Web;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
@@ -15,55 +15,8 @@ namespace SportsStore.UnitTests
     /// Summary description for PaginateTest
     /// </summary>
     [TestClass]
-    public class PaginateTest
+    public class PaginationTest
     {
-        public PaginateTest()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
-
         [TestMethod]
         public void Can_Paginate()
         {
