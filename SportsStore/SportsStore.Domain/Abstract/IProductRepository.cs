6f3518469c334e0ad6d851a94d1e368456ee730e
Lasse Westerberg <westerberg.lasse@gmail.com>
2012-09-16 15:36:15 +0200

    Commit #1

---
+++ SportsStore/SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SportsStore.Domain.Abstract
+{
+    interface IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
