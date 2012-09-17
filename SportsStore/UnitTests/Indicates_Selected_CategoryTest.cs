2edfa7f4b64a97848434db7924bfd36f2901cd38
westerberg.lasse <westerberg.lasse@gmail.com>
2012-09-17 14:26:55 +0200

    Indicates_Selected_CategoryTest

---
+++ SportsStore/UnitTests/Indicates_Selected_CategoryTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using Moq;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Summary description for Indicates_Selected_CategoryTest
+    /// </summary>
+    [TestClass]
+    public class Indicates_Selected_CategoryTest
+    {
+        [TestMethod]
+        public void Indicates_Selected_Category()
+        {
+            // Arrange
+            // - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductID = 4, Name = "P2", Category = "Oranges"},
+            }.AsQueryable());
+            // Arrange - create the controller
+            NavController target = new NavController(mock.Object);
+            // Arrange - define the category to selected
+            string categoryToSelect = "Apples";
+            // Action
+            string result = target.Menu(categoryToSelect).ViewBag.SelectedCategory;
+            // Assert
+            Assert.AreEqual(categoryToSelect, result);
+        }
+    }
+}
