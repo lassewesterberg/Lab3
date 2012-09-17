bd0a74def0b5c447c56f2f32440b4956720c003f
westerberg.lasse <westerberg.lasse@gmail.com>
2012-09-17 20:15:19 +0200

    Calculate_Cart_TotalTest

---
+++ SportsStore/UnitTests/Calculate_Cart_TotalTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
+using Moq;
+
+namespace UnitTests {
+	/// <summary>
+	/// Summary description for Calculate_Cart_TotalTest
+	/// </summary>
+	[TestClass]
+	public class Calculate_Cart_TotalTest {
+
+		[TestMethod]
+		public void Calculate_Cart_Total() {
+
+			// Arrange - create some test products 
+			Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+			Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+			// Arrange - create a new cart 
+			Cart target = new Cart();
+
+			// Act 
+			target.AddItem(p1, 1);
+			target.AddItem(p2, 1);
+			target.AddItem(p1, 3);
+			decimal result = target.ComputeTotalValue();
+
+			// Assert 
+			Assert.AreEqual(result, 450M);
+		} 
+	}
+}
