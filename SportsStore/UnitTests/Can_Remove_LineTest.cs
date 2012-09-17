3fcce013a5b323c2a1b820e0829a55662eb39382
westerberg.lasse <westerberg.lasse@gmail.com>
2012-09-17 20:08:42 +0200

    Can_Remove_LineTest

---
+++ SportsStore/UnitTests/Can_Remove_LineTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
+using Moq;
+
+namespace UnitTests {
+	/// <summary>
+	/// Summary description for Can_Remove_LineTest
+	/// </summary>
+	[TestClass]
+	public class Can_Remove_LineTest {
+		
+		[TestMethod] 
+		public void Can_Remove_Line() { 
+
+			// Arrange - create some test products 
+			Product p1 = new Product { ProductID = 1, Name = "P1" }; 
+			Product p2 = new Product { ProductID = 2, Name = "P2" }; 
+			Product p3 = new Product { ProductID = 3, Name = "P3" }; 
+ 
+			// Arrange - create a new cart 
+			Cart target = new Cart(); 
+			// Arrange - add some products to the cart 
+			target.AddItem(p1, 1); 
+			target.AddItem(p2, 3); 
+			target.AddItem(p3, 5); 
+			target.AddItem(p2, 1); 
+ 
+			// Act 
+			target.RemoveLine(p2); 
+ 
+			// Assert 
+			Assert.AreEqual(target.Lines.Where(c => c.Product == p2).Count(), 0); 
+			Assert.AreEqual(target.Lines.Count(), 2); 
+		} 
+	}
+}
