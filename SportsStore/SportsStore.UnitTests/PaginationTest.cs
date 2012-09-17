19716531e1827fa31d21d9dcbb455489cf518d78
Lasse Westerberg <westerberg.lasse@gmail.com>
2012-09-17 07:54:50 +0200

    Pagination Test - update

--- SportsStore/SportsStore.UnitTests/PaginationTest.cs
+++ SportsStore/SportsStore.UnitTests/PaginationTest.cs
@@ -7,12 +7,13 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
 using Moq;
 
 namespace SportsStore.UnitTests
 {
     /// <summary>
-    /// Summary description for PaginateTest
+    /// Summary description for PaginationTest
     /// </summary>
     [TestClass]
     public class PaginationTest
@@ -24,19 +25,19 @@ namespace SportsStore.UnitTests
             // - create the mock repository
             Mock<IProductRepository> mock = new Mock<IProductRepository>();
             mock.Setup(m => m.Products).Returns(new Product[] {
-            new Product {ProductID = 1, Name = "P1"},
-            new Product {ProductID = 2, Name = "P2"},
-            new Product {ProductID = 3, Name = "P3"},
-            new Product {ProductID = 4, Name = "P4"},
-            new Product {ProductID = 5, Name = "P5"}
-        }.AsQueryable());
+           new Product {ProductID = 1, Name = "P1"},
+           new Product {ProductID = 2, Name = "P2"},
+           new Product {ProductID = 3, Name = "P3"},
+           new Product {ProductID = 4, Name = "P4"},
+           new Product {ProductID = 5, Name = "P5"}
+       }.AsQueryable());
             // create a controller and make the page size 3 items
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
             // Action
-            IEnumerable<Product> result = (IEnumerable<Product>)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
             // Assert
-            Product[] prodArray = result.ToArray();
+            Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
             Assert.AreEqual(prodArray[0].Name, "P4");
             Assert.AreEqual(prodArray[1].Name, "P5");
