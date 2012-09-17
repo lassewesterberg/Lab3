f4c37ef08006850fc76156ec91aa0e211550fe79
westerberg.lasse <westerberg.lasse@gmail.com>
2012-09-17 13:03:57 +0200

    Refining Category URLs

--- SportsStore/SportsStore.WebUI/Global.asax.cs
+++ SportsStore/SportsStore.WebUI/Global.asax.cs
@@ -20,18 +20,32 @@ namespace SportsStore.WebUI
 
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.MapRoute(
-                null, 
-                "Page{page}",
-                new { Controller = "Product", action = "List" }
-            );
-
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.MapRoute(
-            "Default", // Route name
-            "{controller}/{action}/{id}", // URL with parameters
-            new { controller = "Product", action = "List", id = UrlParameter.Optional }
+            routes.MapRoute(null,
+            "", // Only matches the empty URL (i.e. /)
+            new
+            {
+                controller = "Product",
+                action = "List",
+                category = (string)null,
+                page = 1
+            }
             );
+            routes.MapRoute(null,
+            "Page{page}", // Matches /Page2, /Page123, but not /PageXYZ
+            new { controller = "Product", action = "List", category = (string)null },
+            new { page = @"\d+" } // Constraints: page must be numerical
+            );
+            routes.MapRoute(null,
+            "{category}", // Matches /Football or /AnythingWithNoSlash
+            new { controller = "Product", action = "List", page = 1 }
+            );
+            routes.MapRoute(null,
+            "{category}/Page{page}", // Matches /Football/Page567
+            new { controller = "Product", action = "List" }, // Defaults
+            new { page = @"\d+" } // Constraints: page must be numerical
+            );
+            routes.MapRoute(null, "{controller}/{action}");
         }
 
         protected void Application_Start()
