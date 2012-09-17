08cd7c5f8d8e1ea44a2a2449672202d2bc5ee53e
westerberg.lasse <westerberg.lasse@gmail.com>
2012-09-17 19:55:51 +0200

    Can_add_New_LinesTest (this is the one)

--- SportsStore/UnitTests/Can_Add_New_Lines(Test.cs
+++ SportsStore/UnitTests/Can_Add_New_Lines(Test.cs
@@ -36,27 +36,5 @@ namespace UnitTests {
 			Assert.AreEqual(results[0].Product, p1); 
 			Assert.AreEqual(results[1].Product, p2); 
 		} 
-		
-		[TestMethod] 
-		public void Can_Add_Quantity_For_Existing_Lines() { 
- 
-			// Arrange - create some test products 
-			Product p1 = new Product { ProductID = 1, Name = "P1" }; 
-			Product p2 = new Product { ProductID = 2, Name = "P2" }; 
- 
-			// Arrange - create a new cart 
-			Cart target = new Cart(); 
- 
-			// Act 
-			target.AddItem(p1, 1); 
-			target.AddItem(p2, 1); 
-			target.AddItem(p1, 10); 
-			CartLine[] results = target.Lines.OrderBy(c => c.Product.ProductID).ToArray(); 
- 
-			// Assert 
-			Assert.AreEqual(results.Length, 2); 
-			Assert.AreEqual(results[0].Quantity, 11); 
-			Assert.AreEqual(results[1].Quantity, 1); 
-		}
 	}
 }
