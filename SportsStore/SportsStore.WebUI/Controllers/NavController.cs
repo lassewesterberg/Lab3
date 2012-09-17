ad52413509c95d5173b707849a16295ecc63b156
westerberg.lasse <westerberg.lasse@gmail.com>
2012-09-17 13:35:35 +0200

    Navigation Controller

---
+++ SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IProductRepository repository;
+        public NavController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x);
+            return PartialView(categories);
+        }
+    }
+}
