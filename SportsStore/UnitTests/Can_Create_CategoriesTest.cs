91962c707ac30a7a4398747e7bb62c7f022368cc
westerberg.lasse <westerberg.lasse@gmail.com>
2012-09-17 13:44:34 +0200

    Can_Create_CategoriesTest

---
+++ SportsStore/UnitTests/Can_Create_CategoriesTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using SportsStore.WebUI.Models;
+using Moq;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Summary description for Can_Create_CategoriesTest
+    /// </summary>
+    [TestClass]
+    public class Can_Create_CategoriesTest
+    {
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // Arrange
+            // - create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+                new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+                new Product {ProductID = 4, Name = "P4", Category = "Oranges"},
+            }.AsQueryable());
+            // Arrange - create the controller
+            NavController target = new NavController(mock.Object);
+            // Act = get the set of categories
+            string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+            // Assert
+            Assert.AreEqual(results.Length, 3);
+            Assert.AreEqual(results[0], "Apples");
+            Assert.AreEqual(results[1], "Oranges");
+            Assert.AreEqual(results[2], "Plums");
+        }
+    }
+}
